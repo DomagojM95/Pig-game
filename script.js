dbdb36e3be0b5159748e42f9921d9bcefedd5bab
DomagojM95 <81382671+DomagojM95@users.noreply.github.com>
2022-11-04 19:47:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let curentScore = 0;
+let activePlayer = 0;
 // rolling dice functionality
 
 butnRoll.addEventListener('click', function () {
@@ -28,8 +30,15 @@ butnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // add dice to current score
     curentScore += dice;
+
+    document.getElementById(`current--${activePlayer}`).textContent =
+      curentScore;
     current0El.textContent = curentScore; // CHANGE LATER
   } else {
     // switch to the next player
+
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    curentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
   }
 });
